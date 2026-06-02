e461c53679d3e5e42b7349ce10d6b7f6f1b7563c
hihiroya <1883922+hihiroya@users.noreply.github.com>
2026-06-03 00:13:16 +0900

    シルエットSVGをかなり描き直して、生成画像に近い方向へ寄せました。主役は背の高い外套と高い襟、若手は前傾姿勢と長いリボン、技巧派は細身の立ち姿と台本を読む構図にしています。光面も少し強めました。

--- src/components/actors/ActorSilhouette.tsx
+++ src/components/actors/ActorSilhouette.tsx
@@ -7,35 +7,55 @@ type Props = {
 export function ActorSilhouette({ type }: Props) {
   if (type === 'junior') {
     return (
-      <svg className="silhouette silhouette-junior" viewBox="0 0 150 190" role="img" aria-label="若手のシルエット">
-        <path className="prop" d="M18 151c16-8 32-11 48-7" />
-        <path className="rim" d="M72 38c-19 6-31 22-43 43-8 14-16 23-27 29 27 5 50-4 67-25" />
-        <path d="M78 34c12 0 22 9 22 21 0 10-6 18-15 21l17 16c18-2 31-9 43-22-4 22-17 38-38 48l-3 50H43l14-57-36 16 13-32 31-18c-5-5-8-12-8-20 0-13 9-23 21-23Z" />
-        <path className="arm" d="M86 87c18 5 33 16 49 33l-16 14c-12-14-24-23-37-27Z" />
-        <path className="cloth" d="M52 114c-16 20-21 39-27 61 21-13 39-22 61-29 20 9 35 19 53 31-6-28-14-48-31-64-18 8-37 9-56 1Z" />
+      <svg className="silhouette silhouette-junior" viewBox="0 0 150 214" role="img" aria-label="若手のシルエット">
+        <path className="aura" d="M16 188 55 74 136 44 104 96 139 199 82 160Z" />
+        <path className="prop" d="M23 201c24-10 66-12 103-4" />
+        <path className="ribbon" d="M82 63c23-20 45-28 64-25-12 9-23 21-35 37 14-5 26-5 37 0-22 9-41 21-56 37" />
+        <path className="ribbon ribbon-tail" d="M86 72c26 5 45 17 56 36-22-11-40-13-58-6" />
+        <path className="hair" d="M58 38c4-11 14-18 25-17 4-7 10-10 18-9-2 5-1 10 3 14 8 1 14 5 18 12-7 1-12 4-15 8 2 8 0 16-6 23-10-8-25-10-45-5-4-9-3-18 2-26Z" />
+        <path d="M84 39c13 0 23 10 23 23 0 9-5 17-12 21l12 15 33-10-15 22-30 10-8 41-20 9-16 33H21l23-45 14-42-37 20 7-29 40-23c-6-5-10-13-10-22 0-13 12-23 26-23Z" />
+        <path className="arm" d="M91 93c19 3 35 12 51 27l-14 16c-14-13-28-20-43-22Z" />
+        <path className="arm" d="M62 91c-13 14-29 23-49 28l-4-20c19-5 33-13 42-25Z" />
+        <path className="cloth" d="M61 116c20 8 39 7 58-2-5 21-9 38-10 55l27 25H85l-15-21-35 27 23-43Z" />
+        <path className="paper-cut" d="M52 181c19-14 39-23 61-27M58 122l-31 48M104 118l30 67M76 88c9 11 13 25 12 43" />
+        <path className="accent" d="M57 115c18 10 39 9 61-2M81 89c7 11 10 24 9 41M39 154l31-13" />
       </svg>
     );
   }
+
   if (type === 'skilled') {
     return (
-      <svg className="silhouette silhouette-skilled" viewBox="0 0 150 190" role="img" aria-label="技巧派のシルエット">
-        <path className="prop" d="M21 160h42" />
-        <path className="book" d="M76 84h52v50H76zM86 94h32M86 106h25M86 118h30" />
-        <path d="M74 30c13 0 23 10 23 23 0 10-6 19-15 22l13 26-8 66H36l9-66 16-27c-8-4-13-12-13-21 0-13 12-23 26-23Z" />
-        <path className="arm" d="M84 91c10-6 27-8 47-6l-2 19c-17-1-31 1-44 9Z" />
-        <path className="rim" d="M49 80c-13 15-20 34-20 58" />
+      <svg className="silhouette silhouette-skilled" viewBox="0 0 150 214" role="img" aria-label="技巧派のシルエット">
+        <path className="aura" d="M44 20h83l-13 187H21Z" />
+        <path className="prop" d="M25 201h96M31 170v36M115 165v40" />
+        <path className="hair" d="M62 31c6-12 19-17 32-13 4-7 11-9 20-5-4 5-4 11 0 17 8 3 12 9 12 17-7-2-14-1-20 3-9-7-25-8-45-3-2-6-2-11 1-16Z" />
+        <path d="M84 27c14 0 25 11 25 25 0 11-7 20-16 24l12 28 25 1-5 22-22-4-2 74H45l5-77-16 43-20-8c7-28 19-51 38-69l13-12c-8-5-13-13-13-22 0-14 14-25 32-25Z" />
+        <path className="book" d="M91 86h45v56H91zM101 99h25M101 112h29M101 125h20" />
+        <path className="arm" d="M91 88c10-8 28-11 49-8l-3 21c-19-1-34 1-45 8Z" />
+        <path className="arm" d="M63 90c-15 16-24 36-27 61l-20-6c4-30 16-53 37-70Z" />
+        <path className="cloth" d="M55 108h47l-5 91H37c5-34 11-65 18-91Z" />
+        <path className="paper-cut" d="M49 188h52M52 164h49M57 126l-10 66M96 120l-8 75M62 145h34" />
+        <path className="accent" d="M58 111h39M54 132h44M50 156h48" />
+        <path className="glasses" d="M64 55h16M87 55h16M80 55h7" />
+        <path className="rim" d="M52 83c-15 18-24 43-25 74M110 29c12 16 15 35 8 57" />
       </svg>
     );
   }
+
   return (
-    <svg className="silhouette silhouette-lead" viewBox="0 0 150 190" role="img" aria-label="主役のシルエット">
-      <path className="prop" d="M23 171c18-9 62-12 101-3" />
-      <path className="rim" d="M72 27c-20 16-31 40-33 72-2 28-10 53-24 76" />
-      <path d="M77 23c14 0 25 11 25 25 0 11-7 21-17 24l15 29 15 70H34l16-72 15-28c-9-4-14-13-14-23 0-14 12-25 26-25Z" />
-      <path className="arm" d="M88 82c20 6 33 20 40 42l-19 8c-6-16-15-27-29-32Z" />
-      <path className="arm" d="M67 77c-14 9-21 22-24 38l20 3c2-10 6-18 15-26Z" />
-      <path className="chest-hand" d="M75 86c10 5 16 12 18 21-9 1-17-1-24-7 1-6 3-10 6-14Z" />
-      <path className="cloth" d="M49 103c-13 29-19 51-25 75h97c-8-30-16-54-29-76-13 8-28 10-43 1Z" />
+      <svg className="silhouette silhouette-lead" viewBox="0 0 150 214" role="img" aria-label="主役のシルエット">
+        <path className="aura" d="M70 9 128 34 108 206H17L42 45Z" />
+        <path className="prop" d="M22 202c25-9 72-12 111-4" />
+        <path className="hair" d="M55 34c4-13 16-22 29-22 5-6 13-8 22-4-4 4-5 9-3 14 9 3 15 10 17 19-8-3-15-2-21 2-10-8-25-9-46-2-1-3 0-5 2-7Z" />
+        <path className="collar" d="M56 72 22 91l23 28 24-34ZM91 73l38 18-20 31-25-36Z" />
+        <path d="M82 22c15 0 27 12 27 27 0 12-8 22-19 26l15 31 24-10-5 34-17 12 25 62H92l-7-52-11 49H31l16-60-26 36 18-76 21-28c-8-5-13-14-13-24 0-15 15-27 35-27Z" />
+        <path className="arm" d="M93 85c19 7 31 23 37 48l-18 9c-6-18-15-30-28-36Z" />
+        <path className="arm" d="M67 82c-15 12-24 27-28 46l21 4c3-13 8-23 18-32Z" />
+        <path className="chest-hand" d="M74 92c10 6 16 14 19 24-10 2-19 0-27-7 1-8 3-13 8-17Z" />
+        <path className="cloth" d="M52 105c-12 33-21 66-30 99h100c-8-40-19-73-32-100-12 8-25 9-38 1Z" />
+      <path className="paper-cut" d="M46 196c25-10 53-11 81-3M45 111c20 13 40 13 60 0M36 171l28-44M100 126l20 70M43 95h24M92 96h28" />
+      <path className="accent" d="M66 86c9 15 12 40 8 75M95 106c-10 10-23 15-37 15M51 72l-16 48M105 78l17 35" />
+      <path className="rim" d="M72 20c-24 22-36 52-38 92-2 35-11 66-27 93M111 36c13 29 15 69 7 120" />
     </svg>
   );
 }
